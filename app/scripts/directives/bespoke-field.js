264f0597b9729d3724e3320e6ba34666a01f351c
Mark Chapman <mark.chapman@gmail.com>
2015-01-23 02:08:03 +0000

    fix(styling) improves styling of email directive

--- app/scripts/directives/bespoke-field.js
+++ app/scripts/directives/bespoke-field.js
@@ -1,52 +1,35 @@
 'use strict';
-websiteApp.directive('emailField', ['$compile', '$filter', 'cssFrameworkService', function ($compile, $filter, cssFrameworkService) {
-  return {
-    restrict: 'E',
-    replace: true,
-    priority: 1,
-    compile: function () {
-      return function (scope, element, attrs) {
-        scope.$watch(attrs.formInput, function () {
-          var info = scope[attrs.schema],
-            template;
-          if (cssFrameworkService.framework() === 'bs2') {
-            template = '<div class="control-group" id="cg_' + info.id + '">';
-            if (!info.label) {
-              info.label = $filter('titleCase')(info.name);
-            }
-            if (info.label !== '') {
-              template += '<label class="control-label" for="' + info.id + '">' + info.label + '</label>';
-            }
-            template += '<div class="controls">' +
-              '<div class="input-prepend">' +
-              '<span class="add-on">@</span>' +
-              '<input type="email" ng-model="record.' + info.name + '" id="' + info.id + '" name="' + info.id + '" />' +
-              '</div>' +
-              '</div>';
-            template += '</div>';
-          } else {
-            template = '<div class="form-group" id="cg_' + info.id + '">';
-            if (!info.label) {
-              info.label = $filter('titleCase')(info.name);
-            }
-            if (info.label !== '') {
-              template += '<label class="col-sm-2" for="' + info.id + '">' + info.label + '</label>';
-            }
-            template += '<div class="col-sm-10">' +
-              '<div class="col-xs-4">' +
-              '<div class="input-group">' +
-              '<span class="input-group-addon input-sm">@</span>' +
-              '<input type="email" class="form-control input-sm" ng-model="record.' + info.name + '" id="' + info.id + '" name="' + info.id + '" />' +
-              '</div>' +
-              '</div>' +
-              '</div>';
-            template += '</div>';
-          }
-
-          element.replaceWith($compile(template)(scope));
-        });
-      };
+websiteApp.directive('emailField', ['$compile', '$filter', 'pluginHelper', 'formMarkupHelper', 'cssFrameworkService',
+  function ($compile, $filter, pluginHelper, formMarkupHelper, cssFrameworkService) {
+    return {
+      restrict: 'E',
+      replace: true,
+      priority: 1,
+      compile: function () {
+        return function (scope, element, attrs) {
+          scope.$watch(attrs.formInput, function () {
+            var info = scope[attrs.schema];
+            var template;
+            var processedAttr = pluginHelper.extractFromAttr(attrs, 'fngUiSelect');
+            template = pluginHelper.buildInputMarkup(scope, attrs.model, processedAttr.info, processedAttr.options, false, false, function (buildingBlocks) {
+              var atSign = {};
+              if (cssFrameworkService.framework() === 'bs2') {
+                atSign.prepend = '<div class="input-prepend"><span class="add-on">@</span>';
+                atSign.append = '</div>';
+              } else {
+                atSign.prepend = '<div class="input-group"><div class="input-group-addon ' + buildingBlocks.compactClass + '">@</div>';
+                atSign.append = '</div>';
+              }
+              return atSign.prepend + formMarkupHelper.generateSimpleInput(
+                  buildingBlocks.common + formMarkupHelper.addTextInputMarkup(buildingBlocks, processedAttr.info, ''),
+                  processedAttr.info,
+                  processedAttr.options
+                ) + atSign.append;
+            });
+            element.replaceWith($compile(template)(scope));
+          });
+        };
+      }
     }
-  };
-}]);
+  }]);
 
