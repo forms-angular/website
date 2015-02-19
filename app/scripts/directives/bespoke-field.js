b3c012b6d68fddffe19037fbea08740d2a92628e
Mark Chapman <mark.chapman@gmail.com>
2015-02-19 16:17:13 +0000

    refactor(example) Refactors example directive

--- app/scripts/directives/bespoke-field.js
+++ app/scripts/directives/bespoke-field.js
@@ -7,29 +7,28 @@ websiteApp.directive('emailField', ['$compile', '$filter', 'pluginHelper', 'form
       priority: 1,
       compile: function () {
         return function (scope, element, attrs) {
-          scope.$watch(attrs.formInput, function () {
-            var info = scope[attrs.schema];
-            var template;
-            var processedAttr = pluginHelper.extractFromAttr(attrs, 'fngUiSelect');
-            template = pluginHelper.buildInputMarkup(scope, attrs.model, processedAttr.info, processedAttr.options, false, false, function (buildingBlocks) {
-              var atSign = {};
-              if (cssFrameworkService.framework() === 'bs2') {
-                atSign.prepend = '<div class="input-prepend"><span class="add-on">@</span>';
-                atSign.append = '</div>';
-              } else {
-                atSign.prepend = '<div class="input-group"><div class="input-group-addon ' + buildingBlocks.compactClass + '">@</div>';
-                atSign.append = '</div>';
-              }
-              return atSign.prepend + formMarkupHelper.generateSimpleInput(
-                  buildingBlocks.common + formMarkupHelper.addTextInputMarkup(buildingBlocks, processedAttr.info, ''),
-                  processedAttr.info,
-                  processedAttr.options
-                ) + atSign.append;
-            });
-            element.replaceWith($compile(template)(scope));
+          var info = scope[attrs.schema];
+          var template;
+          var processedAttr = pluginHelper.extractFromAttr(attrs, 'fngUiSelect');
+          template = pluginHelper.buildInputMarkup(scope, attrs.model, processedAttr.info, processedAttr.options, false, false, function (buildingBlocks) {
+            var atSign = {};
+            if (cssFrameworkService.framework() === 'bs2') {
+              atSign.prepend = '<div class="input-prepend"><span class="add-on">@</span>';
+              atSign.append = '</div>';
+            } else {
+              atSign.prepend = '<div class="input-group"><div class="input-group-addon ' + buildingBlocks.compactClass + '">@</div>';
+              atSign.append = '</div>';
+            }
+            return atSign.prepend + formMarkupHelper.generateSimpleInput(
+                buildingBlocks.common + formMarkupHelper.addTextInputMarkup(buildingBlocks, processedAttr.info, ''),
+                processedAttr.info,
+                processedAttr.options
+              ) + atSign.append;
           });
+          element.replaceWith($compile(template)(scope));
         };
       }
-    }
-  }]);
+    };
+  }]
+);
 
