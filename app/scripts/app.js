f2b2c65f47b0444e56a77abcec35a029a7a3f7bb
Mark Chapman <mark.chapman@gmail.com>
2016-01-13 03:54:15 +0000

    chore(build)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -87,7 +87,7 @@ websiteApp
         $location.path(newPath);
       }
     } else {
-      $css.add(['styles/201601130310app.css']);
+      $css.add(['styles/201601130352app.css']);
     }
     }])
   .controller('CSSSwitchCtrl', ['$location', '$scope', 'cssFrameworkService', '$css', function($location, $scope, cssFrameworkService, $css) {
