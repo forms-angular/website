fdc3cdaeca0ed3f01db2ca0ccd5dd4bb3de65ffe
Mark Chapman <mark.chapman@gmail.com>
2016-02-26 12:35:35 +0000

    chore(depends) Angular 1.5

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -87,7 +87,7 @@ websiteApp
         $location.path(newPath);
       }
     } else {
-      $css.add(['styles/201602031635app.css']);
+      $css.add(['styles/201602261230app.css']);
     }
     }])
   .controller('CSSSwitchCtrl', ['$location', '$scope', 'cssFrameworkService', '$css', function($location, $scope, cssFrameworkService, $css) {
